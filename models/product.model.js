463e8bb6028fbf873cad1d92f21ae073458b7098
Sukru Ozdemir <sukruozdmr97@gmail.com>
2020-07-23 21:42:19 +0300

    :boom: product service added

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import shortid from 'shortid';
+
+import { toJSON, paginate } from './plugins';
 
 const productSchema = new mongoose.Schema(
   {
@@ -12,6 +15,11 @@ const productSchema = new mongoose.Schema(
     },
     prices: [
       {
+        id: {
+          type: String,
+          default: shortid.generate,
+          unique: true,
+        },
         quantity: {
           type: Number,
           required: true,
@@ -22,6 +30,10 @@ const productSchema = new mongoose.Schema(
         },
       },
     ],
+    site: {
+      type: String,
+      required: true,
+    },
     coverImage: {
       data: Buffer,
       contentType: String,
@@ -41,6 +53,32 @@ const productSchema = new mongoose.Schema(
     collation: { locale: 'tr', strength: 1, numericOrdering: true },
   },
 );
+
+// add plugin that converts mongoose to json
+productSchema.plugin(toJSON);
+productSchema.plugin(paginate);
+
+/**
+ * Check if product title is duplicate
+ *
+ * @param {string} title - The product's title
+ * @returns {Promise<boolean>}
+ */
+productSchema.statics.isTitleDuplicate = async function (title) {
+  const province = await this.findOne({ title });
+  return !!province;
+};
+
+/**
+ * Check if product site is duplicate
+ *
+ * @param {string} site - The product's site url
+ * @returns {Promise<boolean>}
+ */
+productSchema.statics.isSiteDuplicate = async function (site) {
+  const product = await this.findOne({ site });
+  return !!product;
+};
 
 const Product = mongoose.model('Product', productSchema);
 
--- services/product.service.js
+++ services/product.service.js
@@ -1,17 +1,121 @@
 import httpStatus from 'http-status';
 
-import { Product } from './../models';
-import { ApiError } from './../utils/api-error';
+import { Product } from '../models';
+import { ApiError } from '../utils/api-error';
 
 /**
- * Create a user
- * @param {Object} userBody
+ * Create a product
+ *
+ * @param {Object} productBody
+ * @returns {Promise<Product>}
+ */
+export const createProduct = async (productBody) => {
+  if (await Product.isTitleDuplicate(productBody.title)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, `"${productBody.title}" ürünü daha önce eklenmiş`);
+  } else if (await Product.isSiteDuplicate(productBody.site)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, `"${productBody.site}" sitesi başka ürüne daha önce eklenmiş`);
+  }
+
+  const product = await Product.create(productBody);
+  return product;
+};
+
+/**
+ * Query for products
+ *
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+export const queryProducts = async (filter, options) => {
+  const products = await Product.paginate(filter, options);
+  return products;
+};
+
+/**
+ * Update product by id
+ *
+ * @param {ObjectId} productId
+ * @param {Object} updateBody
  * @returns {Promise<User>}
  */
-export const createProduct = async (product) => {
-  if (await User.isEmailTaken(userBody.email)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+export const updateProductById = async (productId, updateBody) => {
+  const product = await getProductById(productId);
+
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Ürün bulunamadı');
   }
-  const user = await User.create(userBody);
-  return user;
+
+  if (updateBody.title && (await Product.isTitleDuplicate(updateBody.title))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, `"${productBody.title}" ürünü daha önce eklenmiş`);
+  } else if (updateBody.site && (await Product.isSiteDuplicate(updateBody.site))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, `"${productBody.site}" sitesi başka ürüne daha önce eklenmiş`);
+  }
+
+  Object.assign(product, updateBody);
+
+  await product.save();
+  return product;
+};
+
+/**
+ * Delete product by id
+ *
+ * @param {ObjectId} productId
+ * @returns {Promise<Product>}
+ */
+export const deleteProductById = async (productId) => {
+  const product = await getProductById(productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Ürün bulunamadı');
+  }
+
+  await product.remove();
+  return product;
+};
+
+/**
+ * Add price to product by id
+ *
+ * @param {ObjectId} productId
+ */
+export const addProductPriceById = async (productId, price, quantity) => {
+  const product = await getProductById(productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Ürün bulunamadı');
+  }
+
+  product.prices.push({ price, quantity });
+  await product.save();
+  return product;
+};
+
+/**
+ * Remove price from product by id
+ *
+ * @param {ObjectId} productId
+ * @param {String}   priceId
+ */
+export const removeProductPriceById = async (productId, priceId) => {
+  const product = await getProductById(productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Ürün bulunamadı');
+  }
+
+  product.prices.remove({ id: priceId });
+  await product.save();
+  return product;
+};
+
+/**
+ * Get product by id
+ *
+ * @param {ObjectId} productId
+ * @returns {Promise<Product>}
+ */
+export const getProductById = async (productId) => {
+  return Product.findById(productId);
 };
