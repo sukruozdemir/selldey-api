49fab62641d8268538f988369b1429b00070f6c4
Sukru Ozdemir <sukruozdmr97@gmail.com>
2020-07-23 21:52:15 +0300

    edit services index.js

--- services/index.js
+++ services/index.js
@@ -1,7 +1,10 @@
 import * as Auth from './auth.service';
 import * as Category from './category.service';
+import * as City from './city.service.service';
 import * as OrderStatus from './order-status.service';
+import * as Product from './product.service';
+import * as Province from './province.service';
 import * as Token from './token.service';
 import * as User from './user.service';
 
-export { Auth, Category, OrderStatus, Token, User };
+export { Auth, Category, City, OrderStatus, Product, Province, Token, User };
